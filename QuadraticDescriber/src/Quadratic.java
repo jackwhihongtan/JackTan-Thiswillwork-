4496553ca44e502692aae609da0bc68d9432e921
jackwhihongtan <18tanj98@millshighsch.org>
2017-10-10 10:29:03 -0700

    Part 5

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -1,4 +1,22 @@
 
 public class Quadratic {
+	public static String quadDescriber(int a, int b, int c) {
+		boolean done = false;
+		do {
+			String direction = "";
+			if (a < 0) {
+				direction = "Down";
+			} else {
+				direction = "Up";
+			}
+				double aos = -1 * b / (2 * a);
+				double vertexX = aos;
+				double vertexY = (4 * a * c - b * b)/ (4 * a);
+				double xcepts = 
+				double ycepts = c;
+		while (done = true); 
+		
+			}
+		}	
+	}
 
-}
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
 
 public class QuadraticClient {
-
+	public static void main(String[] args) {
+		Scanner userinput = new Scanner(System.in);
+		System.out.println("Please type three integers a, b, c");
+		int a = userinput.nextInt();
+		int b = userinput.nextInt();
+		int c = userinput.nextInt();
+		
+	}
 }

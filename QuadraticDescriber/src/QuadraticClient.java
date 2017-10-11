efde716fc11d26261bd830990bc7eecf2d6fe5ff
jackwhihongtan <18tanj98@millshighsch.org>
2017-10-10 21:48:13 -0700

    Some changes

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class QuadraticClient {
 	public static void main(String[] args) {
 		Scanner userinput = new Scanner(System.in);
-		System.out.println("Please type three integers a, b, c");
+		System.out.println("Please type three integers a: ");
 		int a = userinput.nextInt();
+		System.out.println("Please type three integers b: ");
 		int b = userinput.nextInt();
+		System.out.println("Please type three integers c: ");
 		int c = userinput.nextInt();
 		
 	}

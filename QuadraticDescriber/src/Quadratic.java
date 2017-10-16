d6a69a0c94c7355f9b1f3ec82eaad2e0f2e2b524
jackt <jackt@LAPTOP-4EVBE05A.attlocal.net>
2017-10-15 22:00:54 -0700

    Did most of Part 5

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -1,22 +1,62 @@
 
 public class Quadratic {
+	public static int discriminant (int a, int b, int c) { 
+		return b * b - 4 * a * c;
+	}
+	public static double round2 (double x) {
+		double x_hundred = x * 100; //shift x two decimals to right
+		double hundred = x_hundred % 1;
+		double round = (x_hundred - hundred); // takes out the same numbers and leave the thousands place we need
+			if (hundred >= 0.5) { //test for if we need to increment
+				round += 1;
+			} else if (hundred <= -0.5) { //test for if we need to decrement
+				round -= 1;
+			}
+			double rounded = round/100; //moves the value 2 places back to the left
+				return rounded;
+	}
+	public static double sqrt (double num) {
+		if (num < 0) {
+			throw new IllegalArgumentException ("Undefined"); //free check because anything less than zero this program execute
+		}
+		double squareRoot = 1;
+			while (squareRoot * squareRoot > (num + .1) || squareRoot * squareRoot < (num - .1)) { 
+				while (squareRoot * squareRoot < num) {
+					squareRoot = squareRoot + .01; // to make the value in between the require boundary
+				}
+					if (squareRoot * squareRoot > num) {
+							squareRoot = squareRoot - .01;
+				}
+			}
+				return (Quadratic.round2(squareRoot)); //rounds it to make sure we don't have 3 demical places
+	}
+	
+
 	public static String quadDescriber(int a, int b, int c) {
 		boolean done = false;
-		do {
-			String direction = "";
-			if (a < 0) {
-				direction = "Down";
-			} else {
-				direction = "Up";
-			}
-				double aos = -1 * b / (2 * a);
-				double vertexX = aos;
-				double vertexY = (4 * a * c - b * b)/ (4 * a);
-				double xcepts = 
-				double ycepts = c;
-		while (done = true); 
-		
-			}
-		}	
+		double ycept = 0;
+		double xcept1 = 0;
+		double xcept2 = 0;
+		double discriminant = b * b - 4 * a * c;
+		double aos = 0;
+		double vertexY = 0;
+		String direction = "";
+		if (a < 0) {
+			direction = "Down";
+		} else {
+			direction = "Up";
+		}
+		aos = -1 * b / (2 * a);
+		vertexY = (4 * a * c - b * b)/ (4 * a);
+		if (discriminant <= 0) {
+			String xcept = "Does not exist";
+		}else {
+			xcept1 = (-1 * a + (Quadratic.sqrt(Quadratic.discriminant(a, b, c) / (2 * a)))); //but put discrim in sqrt for the square root  
+			xcept2 = (-1 * a - (Quadratic.sqrt(Quadratic.discriminant(a, b, c) / (2 * a))));
+		}
+		ycept = c;
+		return "Opens: " + direction + "/n Aos: " + aos + "/n Vertex: " + "(" +aos + "," + vertexY +")" + "/n X-Intercept: " + xcept1 + "," + xcept2 + "/n Y-Intercept: " + ycept +""; 
 	}
+}	
+	
 
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -2,13 +2,24 @@ import java.util.Scanner;
 
 public class QuadraticClient {
 	public static void main(String[] args) {
-		Scanner userinput = new Scanner(System.in);
-		System.out.println("Please type three integers a: ");
-		int a = userinput.nextInt();
-		System.out.println("Please type three integers b: ");
-		int b = userinput.nextInt();
-		System.out.println("Please type three integers c: ");
-		int c = userinput.nextInt();
-		
+		boolean notDone = false;
+			while (notDone = false) {
+				Scanner userinput = new Scanner(System.in);
+				System.out.println("Please type three integers a, b, c: ");
+				System.out.println("a");
+				int a = userinput.nextInt();
+				System.out.println("b:");
+				int b = userinput.nextInt();
+				System.out.println("c:");
+				int c = userinput.nextInt();
+				System.out.println("Your equation is ax^2 + bx + c");
+				String result = Quadratic.quadDescriber(a, b, c);
+				System.out.println("Are you done?");
+				String doneyet = userinput.nextLine();
+				if (doneyet.charAt(0) == 'q' || doneyet.charAt(0) == 'Q'){
+					notDone = true;
+				}
+			}
+		}
 	}
-}
+

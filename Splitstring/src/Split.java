6baa9833ea83efd939c20f1c6e5220a89e6aa05b
jackt <jackt@LAPTOP-4EVBE05A.attlocal.net>
2017-10-28 14:05:12 -0700

    Done

--- Splitstring/src/Split.java
+++ Splitstring/src/Split.java
@@ -1,33 +1,30 @@
 import java.util.Arrays;
-
+//Jack Tan 10/28/17
 
 
 public class Split {
 	public static void main(String[] args) {
-		// Your task Part 0
-		//String.split();
-		//It's a method that acts on a string, <StringName>.split(<String sp>);
-		//Where sp is the string where the string splits
-		//And it returns an array
-		// Example: "I like apples!".split(" "); 
-//			it will split at spaces and return an array of ["I","like","apples!"]
-		// Example 2: "I really like really red apples".split("really")
-//			it will split at the word "really" and return an array of ["I "," like "," red apples!"]
-		//play around with String.split! 
-		//What happens if you "I reallyreally likeapples".split("really") ?
-		// System.out.println(Arrays.toString("I like apples!
-		// premake an array
-		String[] word = "really like really red apples!".split("really");
-		System.out.println(Arrays.toString(word));
+		String[] original = Getthefilling ("applespineapplesbreadlettusbreadtomatobaconmayohambreadcheese");
+		System.out.println(Arrays.toString(original));
 	}	
 	//precode for part 1
-			// make a method called gettheFilling make an array make a for loop to test for each word 
+			// make a method called gettheFilling make an array make a  test for each word 
 			// when we hit the bread we stop but also check for more bread.
 			// check for the letter r and if bread in present we then return whats in the middle.
-	public static String Getthefilling (String filling) {
-		boolean run = true;
-		if ()
-		String[] filling = "";
+	public static String[] Getthefilling (String filling) {
+		String middle = filling;
+		if (filling.indexOf("bread")!= 0) {
+			int outside1 = filling.indexOf("bread");
+			int outside2 = filling.lastIndexOf("bread");
+			middle = filling.substring(outside1 + 5, (outside2));
+		}
+		String[] finish = middle.split("bread");
+		
+		return finish;
+		
+	
+	
+		
 		
 		
 	}

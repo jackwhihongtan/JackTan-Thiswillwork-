4c1ff2c80878a866f4f175dcb2b3592f49eef0cc
jackwhihongtan <18tanj98@millshighsch.org>
2017-11-02 09:51:36 -0700

    dsdf

--- Array Mini-Lab/src/LotsOfCopies.java
+++ Array Mini-Lab/src/LotsOfCopies.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class LotsOfCopies {
 	public static void main(String[] args) {
@@ -5,26 +6,27 @@ public class LotsOfCopies {
 		String strMain = "APCS";
 		int[] arrMain = {1, 2, 3, 4, 5};
 		changeMe(num,strMain, arrMain);
-		System.out.println(num);
-		System.out.println(strMain);
-		System.out.println(arrMain);
+		System.out.println("In Main, num:" + num);
+		System.out.println("In Main, strMain:" + strMain);
+		System.out.println("In Main, arrMain:" + Arrays.toString(arrMain));
 		
 	}
 	public static void changeMe(int a, String str, int[] arr) {
+		System.out.println("In changeNum, after the change, num:" + a);
 		a = 5;
+		System.out.println("In changeNum, after the change, num:" + a);
 		int b = a;
+		System.out.println("In changeNum, after the change, num:" + a);
 		a = 8;
-		System.out.println(a + " and " +  b);
 		
-		String start = str + "needs help";
-		String newB = start;
-		String Value = str + "needs help help";
-		System.out.println(Value + " and " +  newB);
+		System.out.println("In changeNum, after the change, strMain:" + str);
+		str = "needs help";
+		System.out.println("In changeNum, after tihe change, strMain:" + str);
+		String newString = str;
+		System.out.println("In changeNum, after the change, strMain:" + str);
 		
+		System.out.println("In changeNum, after the change, arr:" + Arrays.toString(arr));
 		arr[0] = 8 ;
-		int c = arr[0];
-		arr[0] = 6;
-		System.out.println(arr[0] + " and " + c);
 		
 		
 		
